990ad3ab41bf03982eb31d7dc4d94ca119f0e6b6
Korbinian Ludwig <korbinian.ludwig06@gmail.com>
2026-03-26 10:35:38 +0100

    refactor: split code into methods

--- Main.java
+++ Main.java
@@ -5,17 +5,22 @@ public class Main {
     Scanner scanner = new Scanner(System.in);
     System.out.println("How many Grades do you want to put in?");
     int numGrades = scanner.nextInt();
-    double[] grades = new double[numGrades];
-    for (int i = 0; i < numGrades; i++) {
-      System.out.println("Give me a grade!");
-      grades[i] = scanner.nextDouble();
-    }
+    double[] grades = getGrades(numGrades, scanner);
+    double average = calculateAverage(grades);
+    printResult(average);
+    scanner.close();
+  }
+
+  static double calculateAverage(double[] grades) {
     double total = 0.0;
-    for (int i = 0; i < numGrades; i++) {
+    for (int i = 0; i < grades.length; i++) {
       total = total + grades[i];
     }
-    double average = total / numGrades;
-    System.out.println("You entered " + total + " grades!");
+    double average = total / grades.length;
+    return average;
+  }
+
+  static void printResult(double average) {
     System.out.println(average + " is your grade average!");
     if (average < 4.5) {
       System.out.println("You passed the class!");
@@ -24,6 +29,14 @@ public class Main {
     } else {
       System.out.println("The teachers need to discuss if you pass the class or not!");
     }
-    scanner.close();
+  }
+
+  static double[] getGrades(int numGrades, Scanner scanner) {
+    double[] grades = new double[numGrades];
+    for (int i = 0; i < numGrades; i++) {
+      System.out.println("Give me a grade!");
+      grades[i] = scanner.nextDouble();
+    }
+    return grades;
   }
 }
